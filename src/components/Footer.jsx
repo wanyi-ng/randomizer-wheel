966f890c6535d0808ef20c5fad43bc2630f00635
Wanyi Ng <wanyi.ng1975@gmail.com>
2026-03-14 16:05:46 -0400

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="bg-indigo-700 p-8">
       <div className="container mx-auto w-full flex items-center justify-between">
         <p className="text-white text-center xl:text-left">
-          &copy; 2023 Randomizer Wheel.
+          &copy; 2023 Randomizer Wheel. All Rights Reserved.
         </p>
         <NavLink to="https://github.com/wanyi-ng" className="underline">
           Github
